49aac6fd8a7df3eab8a18ca5cac861d15eb20c42
l-a-newman <82329904+l-a-newman@users.noreply.github.com>
2021-05-04 16:32:36 -0400

    Add files via upload

--- gym-air_rev/air_rev/envs/ar_env.py
+++ gym-air_rev/air_rev/envs/ar_env.py
@@ -2,16 +2,25 @@ import gym
 import numpy as np
 import sys
 import copy
+import math
 
 class AirRev(gym.Env):
 
-    def __init__(self, load=0.5, l =4):
+    def __init__(self, l =3, epoch = 20, capVal = 2.0):
+        # hi-lo demand hyperparams
+        self.a = 5
+        self.rho = 1
+
+        # checks if we should end
+        self.done = False
+
         self.m = 2 * l
         self.n = 2 * (l**2 + l)  # slide 11 from lecture 5 has a typo (should be + instead of -)
         num_iter = int(self.n/2)
 
-        capa = [1.0]*self.m
-        self.capacities = np.array(capa)  
+        self.timeSteps = 0
+        self.capa = [capVal]*self.m
+        self.capacities = np.array(self.capa)  
         self.demands = np.identity(self.m)
         for i in range(l):
             for j in range(l):
@@ -21,16 +30,22 @@ class AirRev(gym.Env):
                     demand_col[2 * j] = 1.0
                     self.demands = np.append(self.demands, demand_col, axis = 1)
         self.demands = np.append(self.demands, self.demands, axis = 1)
-        self.revenue = np.asarray([1.0]*self.m + [2.0]*(self.n-self.m)+[2.0]*self.m + [4.0]*(self.n-self.m))
+        lowFares = np.random.randint(15,50,self.n//2)
+        self.revenue = np.append(lowFares, 5*lowFares)
+        print(self.revenue)
         #self.revenue = np.asarray([[1.0,2.0][j] for j in range(2) for i in range(num_iter)])
-        self.epoch = 50  # the number of time steps we have to finish within
-        self.probabilities = np.asarray([[3/4*1.8/self.n,1/4*1.8/self.n][j] for j in range(2) for i in range(num_iter)] + [0.1])
+        self.epoch = epoch  # the number of time steps we have to finish within
+        itineraryDemands = np.random.uniform(0,1,self.n//2)
+        scaleTerm = sum(itineraryDemands)
+        self.itinDemds = 0.8*itineraryDemands/scaleTerm
+        demdsWoNoArrival = np.append(0.75*self.itinDemds, 0.25*self.itinDemds)
+        self.probabilities = np.append(demdsWoNoArrival, np.asarray(0.2))
         # the final entry accounts for the probability of no arrival
 
 
-        self.state = np.array(capa)  # Start at beginning of the chain
+        self.state = np.array(self.capa)  # Start at beginning of the chain
         self.action_space = gym.spaces.MultiBinary(self.n)
-        self.observation_space = gym.spaces.MultiDiscrete([2.0]*self.m) #capa + 1 since strictly less than
+        self.observation_space = gym.spaces.MultiDiscrete([capVal + 1]*self.m) #capa + 1 since strictly less than
         self.seed()
         metadata = {'render.modes': ['ansi']}
 
@@ -39,56 +54,68 @@ class AirRev(gym.Env):
         return [seed]
 
     def step(self, action):
-        assert self.action_space.contains(action)
+        if not self.done:
+            self.timeSteps += 1
 
-        self.reward = 0.0
+            # change probabilities
+            # log base a
+            # hiFareDelta = math.log(self.a+(self.epoch - self.timeSteps)*self.rho, self.a)
+            # lowFareDelta = math.log(self.a+(self.timeSteps - 1)*self.rho, self.a)
+            # demdsWoNoArrival = np.append(lowFareDelta*self.itinDemds, hiFareDelta*self.itinDemds)
+            # self.probabilities = np.append(demdsWoNoArrival, np.asarray(0.2))
 
-        activity = np.random.choice(range(self.n+1), 1, list(self.probabilities))[0]
-        # if we don't have the action correlated with "no customer arriving"
-        if activity != self.n:
+            assert self.action_space.contains(action)
 
-            if action[activity] == 1:
-                # activity is what class of customer did arrive
-                # demands for this class is the activity'th column
-                dems = self.demands[:,activity]
-                # check if we can book this customer
-                bookable = True
-                # check all demands
-                for i in range(len(dems)):
-                    # check there is enough seats on flight to meet demand
-                    if self.state[i] < dems[i]:
-                        bookable = False
-                if bookable:
-                    #print("-----")
-                    #print(self.state)
-                    #print(dems)
-                    #print("-----")
+            reward = 0.0
 
-                    self.state -= dems
-                    self.reward = self.revenue[activity]
-                    #print(self.reward)
-                else:
+            activity = np.random.choice(range(self.n+1), 1, list(self.probabilities))[0]
+            # if we don't have the action correlated with "no customer arriving"
+            if activity != self.n:
+
+                if action[activity] == 1:
+                    # activity is what class of customer did arrive
+                    # demands for this class is the activity'th column
+                    dems = self.demands[:,activity]
+                    # check if we can book this customer
+                    bookable = True
+                    # check all demands
+                    for i in range(len(dems)):
+                        # check there is enough seats on flight to meet demand
+                        if self.state[i] < dems[i]:
+                            bookable = False
+                    if bookable:
+                        #print("-----")
+                        #print(self.state)
+                        #print(dems)
+                        #print("-----")
+
+                        self.state -= dems
+                        reward = self.revenue[activity]
+                        #reward = np.exp(-self.timeSteps/np.sum(self.state))*revThisRound
+                        #print(self.reward)
+                    else:
+                        pass
+                        #print("demand exceed capacity")
+                elif action[activity] == 0:
+                    # Do nothing
                     pass
-                    #print("demand exceed capacity")
-            elif action[activity] == 0:
-                # Do nothing
-                pass
+                else:
+                    # error
+                    print("action space not binary")
             else:
-                # error
-                print("action space not binary")
-        else:
-            pass
-            #print("No customer arrives")
-
-        done = (np.sum(self.state) == 0) # with more complicated demand allocations, we might need additional cases
-        return self.state, self.reward, done, {}
+                pass
+                #print("No customer arrives")
+            self.done = ((np.sum(self.state) == 0) or (self.timeSteps == self.epoch))
+            
+        return self.state, reward, self.done, {}
 
     def render(self, mode='ansi'):
         outfile = sys.stdout if mode == 'ansi' else super(AirRev, self).render(mode=mode)
         outfile.write(np.array2string(self.state))
 
 
-
     def reset(self):
-        self.state = np.array([1.0]*self.m)
+        self.state = np.array(self.capa)
+        self.done = False
+        self.timeSteps = 0
         return self.state
